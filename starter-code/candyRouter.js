8a9af93fb7de6bb887c313fb33e8627aac5a1406
joshrhughes <josh@mtnpine.us>
2017-12-20 18:41:34 -0700

    I think I have get rgoing fine

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,7 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candies = [
+  { name: "Snickers", color: "Brown" },
+  { name: "ChocoTaco", color: "Dark Brown" },
+  { name: "Skittles", color: "Rainbow" }
+];
 
 //What would need to go into candies
 //in order to pass our first test?
@@ -10,6 +14,7 @@ var candies;
 router.get('/', function(req,res) {
 	// What would go here? 
 	// Hint: we want all candies in JSON format
+	res.send(candies);
 });
 
 // Fill out the rest of the routes here
